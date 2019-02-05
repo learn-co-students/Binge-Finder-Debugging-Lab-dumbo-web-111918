f6d175c765215e2db5a9e7f54f59cb6299946027
github username <patcarrasco@outlook.com>
2019-02-05 12:27:35 -0500

    Done.

--- src/Components/Episode.js
+++ src/Components/Episode.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const Episode = (props) => {
-  let { myEpisode } = props
-
+  let myEpisode = {...props.eachEpisode}
   return (
     <div>
       Episode {myEpisode.number} - {myEpisode.name}
--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import {Grid} from 'semantic-ui-react';
+import TVShow from './TVShow'
 
 class TVShowList extends Component {
 
   mapAllShows = () => {
+    let props = this.props
     if (!!props.searchTerm){
-      props.shows.map((s) => {
+      return props.shows.map((s) => {
         if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
+          return (<TVShow show={s} key={s.id} selectShow={props.selectShow}/>) 
         }
       })
     }
